b3e88f0881a647646db8685f605f30f64b616cc2
Francisco Beccaria <fbeccaria24@gmail.com>
2020-12-14 22:23:54 -0300

    Código actualizado.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// TODO: Replace the following with your app's Firebase project configuration
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 const firebaseConfig = {
   apiKey: 'AIzaSyA-xQH2gBIpnQn1eQAoKCUHxr_9vGa5Pj4',
